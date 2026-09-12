5b4a31c15fcf42a2385e6b8b719e9389b5ac7e1d
codewithreetu <codewithreetu@gmail.com>
2026-09-12 14:03:34 +0530

    save

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const API_CONFIG = {
   BASE_URL: 'https://deepmagic.onrender.com/api',
-  APP_NAME: 'appv2',
-  API_KEY: 'dXNlcjExX19TRVBSQVRPUl9fYXBwdjI=',
+  // APP_NAME: 'appv2',
+  // API_KEY: 'dXNlcjExX19TRVBSQVRPUl9fYXBwdjI=',
   REGION: 'US',
   APP_VERSION: 'V2',
   ROOT_SAGEION_ID: null, // Add this if you have a specific room/section ID
